3d201c7a48f86f76a39aba65ae992e2f274db446
Pratham22102001 <pratham.guptapg22@gmail.com>
2024-09-15 01:16:21 +0530

    done

--- Interview/Objects.js
+++ Interview/Objects.js
@@ -25,8 +25,52 @@
 // };
 // console.log(user2.firstName);
 
+// const user = {
+//   name: "Pratham",
+//   age: 24,
+//   isTotallygood: true,
+// };
+// for (key in user) {
+//   console.log(user[key]);
+// }
+
+// const user2 = {
+//   a: "A",
+//   b: "b",
+//   a: "c",
+// };
+// console.log(user2);
+// IF TWO KEYS ARE SAME THEN LAST KEY WILLBE PRINTED
+
+//CREATE A FUNCTION MULTIPLIER THAT MULTIPLIES ALL NUMERIC PROPERTY BY 2
+// let nums = {
+//   a: 100,
+//   b: 200,
+//   c: "My nums",
+// };
+
+// function check(nums) {
+//   for (keys in nums) {
+//     if (typeof nums[keys] == "number") {
+//       nums[keys] = nums[keys] * 2;
+//     }
+//   }
+// }
+
+// check(nums);
+// console.log(nums);
+
+//json.stringify and json.parse
 const user = {
-  name: "Pratham",
-  age: 24,
-  isTotallygood: true,
+  name: "test",
+  age: 16,
 };
+
+const obj = JSON.stringify(user);
+// console.log(obj);
+localStorage.setItem("user", JSON.stringify(user));
+//the most way where i can use this is in when storing data in local storage
+
+console.log(localStorage.getItem(JSON.parse("user")));
+
+console.log(JSON.parse(obj));
